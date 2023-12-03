8f90333ea6c55718397f026bf74ecc34dd693528
Joshua Grant <grantjoshua1995@gmail.com>
2023-12-04 00:10:30 +0100

    add the ability to type and show characters on screen

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,30 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	const test = 'Hey';
+	const words = ['One', 'Two', 'Three'];
+
+	let typedCharacters: string[] = [];
+	let timer = 60;
+
+	const handleTypedCharacter = (e: KeyboardEvent) => {
+		typedCharacters.push(e.key);
+		typedCharacters = typedCharacters;
+	};
+
+	// const timerInterval = setInterval(() => {
+	// 	timer = timer - 1;
+	// 	if (timer === 0) {
+	// 		clearInterval(timerInterval);
+	// 	}
+	// }, 1000);
+</script>
+
+<h1>{test}</h1>
+<p>{timer}</p>
+<div on:keypress={handleTypedCharacter} tabindex="0" role="textbox">
+	{#each typedCharacters as character}
+		<span>{character}</span>
+	{/each}
+	{#each words as word}
+		<p>{word}</p>
+	{/each}
+</div>
